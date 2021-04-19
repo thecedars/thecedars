aab70de3bfaac39ebb6ff584e3ca1d8932bc5ca6
Jon Shipman <jon@jonshipman.com>
2021-04-18 21:04:53 -0500

    no footer menu

--- public/includes/menus.php
+++ public/includes/menus.php
@@ -12,7 +12,6 @@
  */
 function cedars_register_menus() {
 	register_nav_menu( 'header-menu', __( 'Header Menu', 'react-build' ) );
-	register_nav_menu( 'footer-menu', __( 'Footer Menu', 'react-build' ) );
 }
 
 add_action( 'after_setup_theme', 'cedars_register_menus' );
