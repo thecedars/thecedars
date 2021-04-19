331f0ab8afdfab4c988ce471d6da0fb26d90f755
Jon Shipman <jon@jonshipman.com>
2021-04-18 22:18:23 -0500

    main background for interior pages.

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useLocation } from "react-router";
 
 export function Main({ children }) {
-  return <div className="main lh-copy relative z-1 flex-auto">{children}</div>;
+  const { pathname } = useLocation();
+
+  if ("/" === pathname) {
+    return (
+      <div className="main lh-copy relative z-1 flex-auto">{children}</div>
+    );
+  } else {
+    return (
+      <div className="main lh-copy relative z-1 flex-auto">
+        <div className="mw7 br3 pa4 mv3 center bg-white">{children}</div>
+      </div>
+    );
+  }
 }
 
 export default Main;
