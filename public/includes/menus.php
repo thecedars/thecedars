d7bfc99f88b2ce56a603d288d942c52938174bcd
Jon Shipman <jon@jonshipman.com>
2021-04-20 20:14:57 -0500

    gettext

--- public/includes/menus.php
+++ public/includes/menus.php
@@ -11,7 +11,7 @@
  * @return void
  */
 function cedars_register_menus() {
-	register_nav_menu( 'header-menu', __( 'Header Menu', 'react-build' ) );
+	register_nav_menu( 'header-menu', __( 'Header Menu', 'cedars' ) );
 }
 
 add_action( 'after_setup_theme', 'cedars_register_menus' );
