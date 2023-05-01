724678018c8fa395a5cb926f0e80ef249fe019ae
Jon Shipman <jon@jonshipman.com>
2023-05-01 12:08:18 -0500

    2023 date adjustments

--- src/meetings/page.js
+++ src/meetings/page.js
@@ -19,39 +19,39 @@ export default function Meetings() {
 
 				<h2>{YEAR} Meeting Schedule</h2>
 
-				<h4>Monday Feb 3</h4>
+				<h4>Monday Feb 6</h4>
 
 				<p>Dwight McQuade</p>
 
-				<h4>Monday Mar 2</h4>
+				<h4>Monday Mar 6</h4>
 
 				<p>Judy Martin</p>
 
-				<h4>Monday Apr 4</h4>
+				<h4>Monday Apr 3</h4>
 
 				<p>Mark Weber</p>
 
-				<h4>Monday May 4</h4>
+				<h4>Monday May 1</h4>
 
 				<p>Mark McPherson</p>
 
-				<h4>Monday Aug 3</h4>
+				<h4>Monday Aug 7</h4>
 
-				<p>Monty</p>
+				<p>Monty Tranbarger</p>
 
-				<h4>Monday Sep 7</h4>
+				<h4>Monday Sep 11</h4>
 
 				<p>Rob Metcalf</p>
 
-				<h4>Monday Oct 5</h4>
+				<h4>Monday Oct 2</h4>
 
 				<p>Mark Weber</p>
 
-				<h4>Thursday Nov 5</h4>
+				<h4>Thursday Nov 2</h4>
 
 				<p>Annual Meeting</p>
 
-				<h4>Monday Dec 7</h4>
+				<h4>Monday Dec 4</h4>
 
 				<p>Jon Shipman</p>
 			</>
