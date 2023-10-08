a23f34bb392f392a6856a5700f4de17ab9e0944a
Jon Shipman <jon@jonshipman.com>
2023-10-08 09:55:16 -0500

    Title inquiry page

--- src/lib/ui/header-nav.svelte
+++ src/lib/ui/header-nav.svelte
@@ -17,6 +17,7 @@
 			<HeaderMenu href="/useful-links">City Links</HeaderMenu>
 		</svelte:fragment>
 	</HeaderMenu>
+	<HeaderMenu href="/title-inquiry">Title Inquiry</HeaderMenu>
 	<HeaderMenu href="/meetings">Meetings</HeaderMenu>
 	<HeaderMenu href="/contact-us">General Contact</HeaderMenu>
 </div>
---
+++ src/routes/(inner)/title-inquiry/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Form from '$lib/form/form.svelte';
+	import Title from '$lib/ui/title.svelte';
+
+	const YEAR = new Date().getFullYear();
+</script>
+
+<Title>Title Inquiry</Title>
+
+<Form inquiry="Title Company" />
