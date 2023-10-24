bb10491906b6e3359d194293941f7578072d3918
Jon Shipman <jon@jonshipman.com>
2023-10-23 19:04:57 -0500

    php linting

--- public/submission/index.php
+++ public/submission/index.php
@@ -6,82 +6,68 @@ require_once __DIR__ . '/class-dot-env.php';
 $env_file = __DIR__ . '/../.env';
 (new DotEnv($env_file))->load();
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: OPTIONS, GET, PATCH, PUT, POST, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, Origin");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: OPTIONS, GET, PATCH, PUT, POST, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, Origin');
 
-$json = file_get_contents( 'php://input' );
+$json = file_get_contents('php://input');
 $data;
 
 try {
-	$data = json_decode( $json, true );
-} catch ( Exception $e ) {
+	$data = json_decode($json, true);
+} catch (Exception $e) {
 	// noop.
 }
 
-if ( empty( $data ) ) {
+if (empty($data)) {
 	return;
 }
 
 $secret = recaptcha_v3_secret();
-if ( ! empty( $secret ) && empty( $data['gToken'] ) ) {
-	http_response_code( 403 );
-	echo json_encode( array( 'error' => 'no recaptcha' ) );
+if (!empty($secret) && empty($data['gToken'])) {
+	http_response_code(403);
+	echo json_encode(['error' => 'no recaptcha']);
 
 	return;
 }
 
 // Check recaptcha.
-if ( ! empty( $secret ) && ! check_recaptcha_token( $data['gToken'] ) ) {
-	http_response_code( 403 );
-	echo json_encode( array( 'error' => 'wrong recaptcha' ) );
+if (!empty($secret) && !check_recaptcha_token($data['gToken'])) {
+	http_response_code(403);
+	echo json_encode(['error' => 'wrong recaptcha']);
 
 	return;
 }
 
-if ( ! empty( $data['gToken'] ) ) {
-	unset( $data['gToken'] );
+if (!empty($data['gToken'])) {
+	unset($data['gToken']);
 }
 
 $message = '';
 
-foreach ( $data as $field => $value ) {
+foreach ($data as $field => $value) {
 	$message .= $field . ":\n" . $value . "\n\n";
 }
 
-mail(
-	form_mail_to(),
-	'The Cedars - Form Submission',
-	$message,
-	array(
-		'From'     => 'website@the-cedars.org',
-		'Reply-to' => $data['email'],
-	)
-);
+mail(form_mail_to(), 'The Cedars - Form Submission', $message, [
+	'From' => 'website@the-cedars.org',
+	'Reply-to' => $data['email'],
+]);
 
 $subject = 'The Cedars - Title Inquiry';
-if ( !empty($data['subject']) ) {
+if (!empty($data['subject'])) {
 	$subject = $data['subject'];
 }
 
 $title_inquiry = form_mail_title_inquiry();
 
-if (
-	!empty($title_inquiry) &&
-	!empty($data['inquiry']) &&
-	$data['inquiry'] === 'Title Company'
-) {
-	mail(
-		$title_inquiry,
-		$subject,
-		$message,
-		array(
-			'From'     => 'website@the-cedars.org',
-			'Reply-to' => $data['email'],
-		)
-	);
+if (!empty($title_inquiry) && !empty($data['inquiry']) && $data['inquiry'] === 'Title Company') {
+	mail($title_inquiry, $subject, $message, [
+		'From' => 'website@the-cedars.org',
+		'Reply-to' => $data['email'],
+	]);
 }
 
-echo json_encode( array( 'data' => $data ) );
+echo json_encode(['data' => $data]);
 
 return;
