85b7c69367a32e9e9c1c55a2b625abc7dc6ac4ea
Jon Shipman <jon@jonshipman.com>
2025-03-05 15:44:14 -0700

    feat: Meeting Schedule change

--- web/src/routes/(inner)/meetings/card.svelte
+++ web/src/routes/(inner)/meetings/card.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div
-	class="rounded-2xl p-4 transition-colors hover:bg-green hover:text-white bg-washed-green flex items-center justify-center cursor-pointer"
+	class="rounded-2xl p-4 transition-colors hover:bg-green hover:text-white bg-washed-green flex items-center justify-center text-center cursor-pointer"
 	on:click
 	on:keypress={handleKeyPress}
 	role="button"
--- web/src/routes/(inner)/meetings/config.ts
+++ web/src/routes/(inner)/meetings/config.ts
@@ -1,16 +1,14 @@
 export const meetings = [
-	{ address: '8409	Greenwood Cir', date: 'Monday Feb 5', name: 'Dwight McQuade' },
-	{ address: '8338	Oakview Cir', date: 'Monday Mar 4', name: 'Judy Martin' },
-	{ address: '8357	Oakview Cir', date: 'Monday Apr 1', name: 'Mark Weber' },
-	{ address: '14521	W. 86th Terr', date: 'Monday May 6', name: 'Mark McPherson' },
-	{ address: '14419	W. 84th Terr', date: 'Monday Aug 5', name: 'Monty Tranbarger' },
-	{ address: '8348	Oakview Cir', date: 'Monday Sep 9', name: 'Rob Metcalf' },
-	{ address: '8357	Oakview Cir', date: 'Monday Oct 7', name: 'Mark Weber' },
+	{ address: '8338	Oakview Cir', date: 'Apr 7', name: 'Vann & Judy Martin' },
+	{ address: '8357	Oakview Cir', date: 'May 5', name: 'Mark Weber' },
+	{ address: '14521	W. 86th Terr', date: 'Aug 4', name: 'Mark McPherson' },
+	{ address: '8348	Oakview Cir', date: 'Sept 8', name: 'Rob Metcalf' },
+	{ address: '?', date: 'Oct 6', name: 'Todd Pratt' },
 	{
 		address: 'The Foundry Church, 8835 Lackman Road, Lenexa, KS 66219',
-		date: 'Thursday Nov 7',
+		date: 'Thursday Nov 6',
 		name: 'Annual Meeting',
 		fulladdress: true
 	},
-	{ address: '14424 W 84th Terr', date: 'Monday Dec 2', name: 'Jon Shipman' }
+	{ address: '8338	Oakview Cir', date: 'Dec 1', name: 'Vann & Judy Martin' }
 ];
