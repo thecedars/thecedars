c71c242233eb54c3ee1d5b4a9f7b0ec03ec06c6c
Shane Starr <sstarr@unitedstatesappraisals.com>
2026-03-19 16:27:05 -0500

    update meeting schedule

--- web/src/routes/(inner)/meetings/config.ts
+++ web/src/routes/(inner)/meetings/config.ts
@@ -1,14 +1,14 @@
 export const meetings = [
-	{ address: '8338	Oakview Cir', date: 'Apr 7', name: 'Vann & Judy Martin' },
-	{ address: '8357	Oakview Cir', date: 'May 5', name: 'Mark Weber' },
-	{ address: '14521	W. 86th Terr', date: 'Aug 4', name: 'Mark McPherson' },
-	{ address: '8348	Oakview Cir', date: 'Sept 8', name: 'Rob Metcalf' },
-	{ address: '?', date: 'Oct 6', name: 'Todd Pratt' },
+	{ address: '8338	Oakview Cir', date: 'Apr 6', name: 'Vann & Judy Martin' },
+	{ address: '8357	Oakview Cir', date: 'May 4', name: 'Mark Weber' },
+	{ address: '14521	W. 86th Terr', date: 'Aug 3', name: 'Mark McPherson' },
+	{ address: '8348	Oakview Cir', date: 'Sept 14', name: 'Rob Metcalf' },
+	{ address: '8358	Oakview Cir', date: 'Oct 5', name: 'Todd Pratt' },
 	{
 		address: 'The Foundry Church, 8835 Lackman Road, Lenexa, KS 66219',
-		date: 'Thursday Nov 6',
+		date: 'Thursday Nov 5',
 		name: 'Annual Meeting',
 		fulladdress: true
 	},
-	{ address: '8338	Oakview Cir', date: 'Dec 1', name: 'Vann & Judy Martin' }
+	{ address: '8338	Oakview Cir', date: 'Dec 7', name: 'Vann & Judy Martin' }
 ];
